d3062945f59b0e340ea0c0b982890436284eeebe
ricardospalves <ricardospalves@gmail.com>
2021-09-14 16:52:24 -0300

    Adicionar os estilos para o Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,3 +1,5 @@
+import styles from './Task.module.css'
+
 const Task = props => {
   const handleDoneChange = () => {
     const data = {
@@ -30,24 +32,36 @@ const Task = props => {
 
   }
 
-  return <li id={props.id}>
-    <input
-      type="checkbox"
-      checked={props.isDone}
-      onChange={handleDoneChange}
-    />
+  return <li id={props.id} className={styles.Task}>
+    <label className={styles.Task__cellToggle}>
+      <span className="u-visuallyHidden">
+        {props.isDone ? 'Reabrir' : 'Concluir'} esta tarefa
+      </span>
 
-    <input
-      type="text"
-      value={props.title}
-      style={{
-        textDecoration: props.isDone ? 'line-through' : ''
-      }}
-      readOnly={props.isDone}
-      onInput={handleTitleInput}
-    />
+      <input
+        type="checkbox"
+        className={styles.Task__toggle}
+        checked={props.isDone}
+        onChange={handleDoneChange}
+      />
+    </label>
+
+    <label className={styles.Task__cellField}>
+      <span className="u-visuallyHidden">
+        Título da tarefa
+      </span>
+
+      <input
+        type="text"
+        value={props.title}
+        className={styles.Task__field}
+        readOnly={props.isDone}
+        onInput={handleTitleInput}
+      />
+    </label>
 
     <button
+      className={styles.Task__delete}
       onClick={handleDeleteClick}
     >
       Deletar
